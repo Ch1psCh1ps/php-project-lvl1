0fc7f47a780e6afd46f65af3cac62f453a2b9fab
Sergey <sergey-sachko97@mail.ru>
2021-08-22 23:24:31 +0300

    new project

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Gre;
+
+use function cli\line;
+use function cli\prompt;
+
+function isGreetings()
+{
+    line("Welcome to the Brain Game!");
+    $name = prompt("May I have your name?");
+    line("Hello, %s!", $name);
+}
