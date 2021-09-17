f1a4e19129bbd4d60a590f8ee46fb2445cf42641
Sergey <sergey-sachko97@mail.ru>
2021-09-17 21:08:21 +0300

    add brain-prime.php

--- src/Games/brain-prime.php
+++ src/Games/brain-prime.php
@@ -2,7 +2,30 @@
 
 namespace App\Prime;
 
+use function App\Gre\isGreetings;
+use function cli\line;
+use function cli\prompt;
+
 function isPrime()
 {
-
+    $name = isGreetings();
+    $result = '';
+    $question = '';
+    for ($i = 0; $i < 3; $i++) {
+        $randomInt = random_int(1, 50);
+        $question = $randomInt;
+        line('Question:' . ' ' . $question);
+        $answer = prompt('Your answer');
+        for ($t = $randomInt; $t > 2; $t--) {
+            if ($randomInt % $t == 0) {
+                $result = $randomInt;
+            }
+        }
+        if ($answer == $result && $answer == 'no') {
+            line('Correct!');
+        } elseif ($answer != $result && $answer == 'yes') {
+            line('Correct!');
+        }
+    }
+    line('Congratulations, ' . "{$name}!");
 }
