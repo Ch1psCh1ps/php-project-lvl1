bf9d870783f06563b2bd52faf53a01dd58435ecf
Sergey <sergey-sachko97@mail.ru>
2021-09-18 19:56:26 +0300

    fixed brain-calc.php

--- src/Games/brain-calc.php
+++ src/Games/brain-calc.php
@@ -9,17 +9,17 @@ use function cli\prompt;
 function itIsRandomCalc()
 {
     $name = isGreetings();
-    $result = '';
     for ($i = 0; $i < 3; $i++) {
+        $result = '';
         $randomInt = random_int(0, 99);
         $randomInt1 = random_int(0, 99);
         $randomArray = ['+', '-', '*'];
         $randomZnak = array_rand($randomArray, 1);
         $randomZnakEnd = $randomArray[$randomZnak];
-        if ($randomZnakEnd === "-") {
+        if ($randomZnakEnd === '-') {
             $result = $randomInt - $randomInt1;
-        } elseif ($randomZnakEnd === "+") {
-            $result = $randomInt - $randomInt1;
+        } elseif ($randomZnakEnd === '+') {
+            $result = $randomInt + $randomInt1;
         } else {
             $result = $randomInt * $randomInt1;
         }
