5aff104f9e50273889003feb92a5d28ff1dcb03a
Sergey <sergey-sachko97@mail.ru>
2021-09-19 00:34:27 +0300

    fixed brain-prime.php part 2

--- src/Games/brain-prime.php
+++ src/Games/brain-prime.php
@@ -6,32 +6,35 @@ use function App\Gre\isGreetings;
 use function cli\line;
 use function cli\prompt;
 
+function verifyPrimeNumber($tally)
+{
+    for ($t = 2; $t <= $tally / 2; $t++) {
+        if ($tally % $t == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function isPrime()
 {
     $name = isGreetings();
     line('Answer "yes" if given number is prime. Otherwise answer "no".');
     for ($i = 0; $i < 3; $i++) {
-        $result = '';
-        $question = '';
-        $randomInt = random_int(1, 50);
+        $randomInt = rand(1, 50);
         $question = $randomInt;
         line('Question:' . ' ' . $question);
         $answer = prompt('Your answer');
-        for ($t = $randomInt; $t > 2; $t--) {
-            if ($randomInt % $t !== 0) {
-                $result = $randomInt;
-            }
+        $result = verifyPrimeNumber($randomInt);
+        if ($result === true) {
+            $trueAnswer = 'yes';
+        } else {
+            $trueAnswer = 'no';
         }
-        if ($answer == $result && $answer === 'no') {
-            line('Correct!');
-        } elseif ($answer != $result && $answer === 'yes') {
-            line('Correct!');
-        } elseif ($answer == $result && $answer === 'no') {
-            line("'{$answer}' is wrong answer ;(. Correct answer was 'yes'.
-Let's try again, {$name}!");
-            die();
-        } elseif ($answer != $result && $answer === 'yes') {
-            line("'{$answer}' is wrong answer ;(. Correct answer was 'no'.
+        if ($answer === $trueAnswer) {
+           line('Correct!');
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$trueAnswer}'.
 Let's try again, {$name}!");
             die();
         }
